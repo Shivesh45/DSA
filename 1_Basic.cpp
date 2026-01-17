98a615830917f149230b3ce3032053356fe51fb5
Shivesh45 <shiveshkumar1011@gmail.com>
2026-01-17 11:17:23 +0530

    basic 1 dsa question

---
+++ 1_Basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Variable declaration
+    int a;
+    //Variable definition and initialization 
+    a = 45;
+
+    cout<<"The value of a is: "<<a<<endl;
+
+    bool b = false;
+    cout<<"The value of b is: "<<b<<endl;
+    return 0;
+}
